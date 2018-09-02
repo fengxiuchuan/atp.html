a0056199680f4c403769dfc2c9c2a0e51a720946
fengxiuchuan <fengxiuchuan91@163.com>
2018-09-02 10:46:24 +0800

    提交菜单管理

---
+++ src/api/req_menu.js
@@ -0,0 +1,10 @@
+// 新增菜单
+const urlAddMenu = '/sysMenu/addMenu.do'
+// 编辑菜单
+const urlEditMenu = '/sysMenu/editMenu.do'
+// 删除菜单
+const urlDelMenu = '/sysMenu/delMenu.do'
+// 查询菜单树
+const urlQueryMenuTree = '/sysMenu/queryMenuTree.json'
+
+export {urlAddMenu, urlEditMenu, urlDelMenu, urlQueryMenuTree}
